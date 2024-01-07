07817ce1d14bff383c6b3b658a2a34234bb5141a
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2024-01-07 18:02:03 +0100

    provider.php. Cosmetic.

--- src/services/provider.php
+++ src/services/provider.php
@@ -2,11 +2,8 @@
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\PluginInterface;
-
 use Joomla\CMS\Factory;
 use Joomla\CMS\Plugin\PluginHelper;
-#use Joomla\CMS\User\UserFactoryInterface;
-#use Joomla\Database\DatabaseInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 use Joomla\Event\DispatcherInterface;
@@ -22,7 +19,7 @@ return new class () implements ServiceProviderInterface {
 		*
 		* @since   4.4.0
 		*/
-	public function register(Container $container): void
+	public function register(Container $container)
 	{
 		$container->set(
 			PluginInterface::class,
